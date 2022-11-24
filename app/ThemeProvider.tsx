60ff9b841e301f4f0e4e03828a61a11007160638
Harry James <harryjamesuk@users.noreply.github.com>
2022-11-24 12:38:09 +0000

    Another good solution using Event Handlers

--- app/ThemeProvider.tsx
+++ app/ThemeProvider.tsx
@@ -1,5 +1,5 @@
 'use client'; // we use state for dark mode toggling, therefore this must be a Client Component.
-import React, {useEffect} from "react";
+import React from "react";
 import DarkModeSwitch from "../components/DarkModeSwitch";
 
 export default function ThemeProvider({
@@ -10,40 +10,42 @@ export default function ThemeProvider({
     const [isDarkMode, setDarkMode] = React.useState(false);
 
     /*
-        Both useEffect and useLayoutEffect are good solutions.
-        Note the differences when using SSR could be of importance:
-        https://gist.github.com/gaearon/e7d97cdf38a2907924ea12e4ebdf3c85
+        Another valid solution is to call our side effect within the event handler. This means we don't have to
+        use onEffect.
+        Note: I'm not saying we should actively avoid onEffect. The docs on 'You Might Not Need an Effect' are a
+        great read: https://beta.reactjs.org/learn/you-might-not-need-an-effect
+        but there are cases (like this one) where it's a good fit.
 
-        Note that useLayoutEffect gives you a warning when using SSR, and must be wrapped in a condition
-        ('if typeof window !== 'undefined'') to avoid it (and this in itself breaks the rules of hooks).
-        This edge case could be acceptable to break this rule, since the check always passes on the client
-        and always fails on the server.
+        We should be careful when writing code like this because it's possible (although unlikely) that the user
+        could 'click multiple times' before the new state is set (since state is set asynchronously).
+        In other words, it's possible we could run our side effects, and have multiple scheduled updates to our state.
 
-        Note the main difference between the two functions is useLayoutEffect will fire the function
-        and process the updates inside it immediately whereas useEffect will fire the function but defer
-        updates.
-        In our case, it makes sense just to use useEffect, since there is no real performance benefit.
-        This is because React will do the following:
-        1. isDarkMode changes, triggers a component re-render.
-        2. React 'commits changes' to the DOM. In fact, at this stage there is no changes as far as React knows
-        (changing isDarkMode never affects the React virtual DOM). ** This is the main reason we can use useEffect
-        instead of useLayoutEffect ** - we never paint, do something that affects the DOM and then paint again, we
-        only paint once ** because the virtual DOM is never affected **.
-        3. useEffect is triggered in the commit phase, React itself never triggers a browser paint because
-        according to React, nothing has changed. However, since our side effect now runs, the browser will
-        perform a browser paint.
+        e.g) Let's say we managed to click 3 times before the state was updated. We would have 3 scheduled updates:
+        setDarkMode(true), setDarkMode(true), setDarkMode(true).
+        This would also mean we've called document.documentElement.classList.add('dark'); 3 times.
+        In our case, this isn't an issue, because we can't add or remove the same class twice, so it's a no-op.
+
+        This is also why we must use the 'newDarkMode' variable for conditions and then set the state afterwards.
+        If we setState() and tried to use that value, our state would be stale, and we'd be using an outdated value,
+        since the state change is scheduled asynchronously.
+
+        It's perfectly valid code, but I think it's a little more readable to use onEffect because you know the state
+        at that time isn't stale.
      */
-    useEffect(() => {
-        if (isDarkMode) {
+    function handleClick() {
+        const newDarkMode = !isDarkMode;
+
+        if (newDarkMode) {
             document.documentElement.classList.add('dark');
         } else {
             document.documentElement.classList.remove('dark');
         }
-    }, [isDarkMode]);
+        setDarkMode(newDarkMode);
+    }
 
     return (
         <>
-            <DarkModeSwitch isDarkMode={isDarkMode} setDarkMode={() => setDarkMode(!isDarkMode)}/>
+            <DarkModeSwitch isDarkMode={isDarkMode} setDarkMode={handleClick}/>
             {children}
         </>
     );
