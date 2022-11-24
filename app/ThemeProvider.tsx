89d416261252f0ccdab6765433bf2e9000d15dd8
Harry James <harryjamesuk@users.noreply.github.com>
2022-11-24 12:11:36 +0000

    A good solution using useEffect()

--- app/ThemeProvider.tsx
+++ app/ThemeProvider.tsx
@@ -1,5 +1,5 @@
 'use client'; // we use state for dark mode toggling, therefore this must be a Client Component.
-import React from "react";
+import React, {useEffect} from "react";
 import DarkModeSwitch from "../components/DarkModeSwitch";
 
 export default function ThemeProvider({
@@ -7,31 +7,43 @@ export default function ThemeProvider({
 }: {
     children: React.ReactNode
 }) {
-    const [isDarkMode, _setDarkMode] = React.useState(false);
+    const [isDarkMode, setDarkMode] = React.useState(false);
 
-    function setDarkMode() {
-        _setDarkMode(prevDarkMode => {
-            const newDarkMode = !prevDarkMode;
+    /*
+        Both useEffect and useLayoutEffect are good solutions.
+        Note the differences when using SSR could be of importance:
+        https://gist.github.com/gaearon/e7d97cdf38a2907924ea12e4ebdf3c85
 
-            /*
-                DO NOT DO THIS - included for demonstration purposes only.
+        Note that useLayoutEffect gives you a warning when using SSR, and must be wrapped in a condition
+        ('if typeof window !== 'undefined'') to avoid it (and this in itself breaks the rules of hooks).
+        This edge case could be acceptable to break this rule, since the check always passes on the client
+        and always fails on the server.
 
-                This is bad because it introduces side effects which *might* affect the render process.
-                (this probably will still work, since setDarkMode is called in an event handler, but
-                it's bad practice anyhow).
-             */
-            if (newDarkMode) {
-                document.documentElement.classList.add('dark');
-            } else {
-                document.documentElement.classList.remove('dark');
-            }
-            return newDarkMode;
-        });
-    }
+        Note the main difference between the two functions is useLayoutEffect will fire the function
+        and process the updates inside it immediately whereas useEffect will fire the function but defer
+        updates.
+        In our case, it makes sense just to use useEffect, since there is no real performance benefit.
+        This is because React will do the following:
+        1. isDarkMode changes, triggers a component re-render.
+        2. React 'commits changes' to the DOM. In fact, at this stage there is no changes as far as React knows
+        (changing isDarkMode never affects the React virtual DOM). ** This is the main reason we can use useEffect
+        instead of useLayoutEffect ** - we never paint, do something that affects the DOM and then paint again, we
+        only paint once ** because the virtual DOM is never affected **.
+        3. useEffect is triggered in the commit phase, React itself never triggers a browser paint because
+        according to React, nothing has changed. However, since our side effect now runs, the browser will
+        perform a browser paint.
+     */
+    useEffect(() => {
+        if (isDarkMode) {
+            document.documentElement.classList.add('dark');
+        } else {
+            document.documentElement.classList.remove('dark');
+        }
+    }, [isDarkMode]);
 
     return (
         <>
-            <DarkModeSwitch isDarkMode={isDarkMode} setDarkMode={setDarkMode}/>
+            <DarkModeSwitch isDarkMode={isDarkMode} setDarkMode={() => setDarkMode(!isDarkMode)}/>
             {children}
         </>
     );
